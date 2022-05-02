0b48b926b24e790175b40fa580eb6e5403b596ff
daddydmitry <104529055+daddydmitry@users.noreply.github.com>
2022-05-02 15:34:08 +0300

    Add files via upload

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Dmitry Sokoltsov on 02.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
