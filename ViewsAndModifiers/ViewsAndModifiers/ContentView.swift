d054222138ea864b0b715b7e51a2641f4216f664
daddydmitry <104529055+daddydmitry@users.noreply.github.com>
2022-05-03 21:03:03 +0300

    Add files via upload

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,124 @@
 
 import SwiftUI
 
-struct ContentView: View {
+// MARk: CapsuleText View
+struct CapsuleText: View {
+    var text: String
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(text)
+            .font(.largeTitle)
             .padding()
+            //.foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(Capsule())
     }
 }
+
+
+//MARK: Custom modifiers
+struct Title: ViewModifier {
+    
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+        .padding()
+            .background((Color.blue))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+    }
+}
+
+
+// MARK: TitlesView for challenge 1
+struct TitlesView: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .font(.largeTitle)
+            .foregroundColor(Color.yellow)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+        
+    }
+}
+// MARK: Struct Watermark
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        GridStack(rows: 4, columns: 4) { row, col in
+          //  HStack {
+            Image(systemName: "\(row * 4 + col).circle")
+                Text("R:\(row) C \(col)")}
+      //  }
+    }
+}
+
+// MARK: GridStack
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) {
+                row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) {
+                        column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
+
+struct ContentView: View {
+    
+
+    var body: some View {
+        
+        Text("Hacking with Swift")
+        .setTitlesView()
+ 
+        }
+    }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// MARK: extension View
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+   
+}
+
+extension View {
+    func setTitlesView() -> some View {
+        self.modifier(TitlesView())
     }
 }
