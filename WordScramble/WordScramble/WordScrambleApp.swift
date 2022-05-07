26e818fc88ca14d607a7dd36c266a4e4577fb086
daddydmitry <104529055+daddydmitry@users.noreply.github.com>
2022-05-07 18:06:22 +0300

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dmitry Sokoltsov on 07.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
