f4f2c9cc2d44edb366df4dfee28ab058c7cfba03
daddydmitry <104529055+daddydmitry@users.noreply.github.com>
2022-05-12 20:21:32 +0300

    Add files via upload

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Dmitry Sokoltsov on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
