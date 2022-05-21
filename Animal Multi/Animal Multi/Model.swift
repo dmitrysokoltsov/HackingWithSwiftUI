98b260ef94930f826a15d9a20be3835cec7c8ce2
daddydmitry <104529055+daddydmitry@users.noreply.github.com>
2022-05-21 23:15:54 +0300

    Model

---
+++ Animal Multi/Animal Multi/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  Animal Multi
+//
+//  Created by Dmitry Sokoltsov on 21.05.2022.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var answer: Int
+}
+
