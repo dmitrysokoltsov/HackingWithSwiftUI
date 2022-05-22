7bd7e60861bfb8be619682122ad6246e0f299a52
daddydmitry <104529055+daddydmitry@users.noreply.github.com>
2022-05-22 20:22:50 +0300

    Add files via upload

---
+++ Animal Multi/Animal Multi/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  Animal Multi
+//
+//  Created by Dmitry Sokoltsov on 22.05.2022.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
