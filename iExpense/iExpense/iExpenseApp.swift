e3c5932ee731f5185c96bef70e27c616c110bdaa
daddydmitry <104529055+daddydmitry@users.noreply.github.com>
2022-05-24 23:26:18 +0300

    Add files via upload

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dmitry Sokoltsov on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
