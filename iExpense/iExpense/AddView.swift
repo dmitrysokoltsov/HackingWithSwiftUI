5b92ee4da299fc365171bc1f0bc56718c606e24e
dmitrysokoltsov <104529055+dmitrysokoltsov@users.noreply.github.com>
2022-06-23 16:48:42 +0300

    Add files via upload

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AddView: View {
     
     @ObservedObject var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
     
     @State private var name = ""
     @State private var type = "Personal"
@@ -35,6 +36,7 @@ struct AddView: View {
                 Button("Save") {
                     let items = ExepenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(items)
+                    dismiss()
                 }
             }
         }
