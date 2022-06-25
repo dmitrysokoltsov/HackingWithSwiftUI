ec1fb48bbee74dac870e0adf522ef6e3773f1090
dmitrysokoltsov <104529055+dmitrysokoltsov@users.noreply.github.com>
2022-06-25 20:50:07 +0300

    Add files via upload

--- Moonshot/Moonshot/Missions.swift
+++ Moonshot/Moonshot/Missions.swift
@@ -11,9 +11,18 @@ struct Missions: Codable, Identifiable {
     struct CrewRole: Codable {
         let name: String
         let role: String
+        
     }
     let id: Int
     let launchDate: String?
     let crew: [CrewRole]
     let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo \(id)"
+    }
 }
